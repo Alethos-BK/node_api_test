6154fde9139d1b037a609c15e98be16f191d11b1
Alethos-BK <odahcamperiles@gmail.com>
2023-10-04 21:51:34 -0300

    configurado com postgres e get já passado, falta as outras rotas

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,7 +1,7 @@
-const mysql = require("mysql2/promise");
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const connection = mysql.createPool({
+const connection = new Pool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
